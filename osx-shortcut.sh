b208c4c95c70288541093f95584554bc33ead69e
drylikov <denis.rylikov@protonmail.com>
2025-11-29 12:41:34 +0300

    osx_shortcut
    Add autocorrect text shortcuts to OS X .

---
+++ osx-shortcut.sh
@@ -0,0 +1,115 @@
+
+#!/bin/sh
+
+#
+# Usage.
+#
+
+usage () {
+cat << EOF
+
+  Usage:
+    shortcut <replace> <with>
+    shortcut (--help | --version)
+
+  Options:
+
+    -h, --help     output usage information
+    -v, --version  output version
+
+  stdin(4), in the CSV format, can be provided instead of arguments.
+
+  Examples:
+
+    $ shortcut "omw" "on my way"
+    $ shortcut <<< "omw,on my way"
+
+  See also: man 1 shortcut
+EOF
+}
+
+#
+# Version.
+#
+
+version () {
+  echo "1.1.0"
+}
+
+#
+# Options.
+#
+
+case $1 in
+  -h|--help)    usage; exit ;;
+  -v|--version) version; exit ;;
+  *)            replace=$1 ; with=$2 ;;
+esac
+
+#
+# Find SQLite database.
+#
+
+database=$(find ~/Library/Dictionaries/CoreDataUbiquitySupport/$USER~*/UserDictionary/*/store/UserDictionary.db | head -1)
+
+if [ "$database" = "" ]; then
+  echo
+  echo "Could not find database."
+  echo "osx-shortcut requires Mavericks or higher."
+  echo "If you are on Mavericks or higher, please raise an issue:"
+  echo
+  echo "  https://github.com/drylikov/osx_shortcut/issues/new"
+  exit 1
+fi
+
+#
+# Find primary key.
+#
+
+primary=$(sqlite3 "$database" 'SELECT Z_MAX from "Z_PRIMARYKEY"')
+
+#
+# Get current time.
+#
+
+now=$(date +%s)
+
+#
+# Insert a shortcut to replace `$1` with `$2`.
+#
+# @param $1 - Replace.
+# @param $2 - With.
+#
+insert() {
+  primary=$((primary+1))
+  sqlite3 "$database" "INSERT INTO \"ZUSERDICTIONARYENTRY\" VALUES($primary,1,1,0,0,0,0,$now,NULL,NULL,NULL,NULL,NULL,'$2','$1',NULL)"
+}
+
+#
+# Insert.
+#
+
+if [ "$replace" != "" ]; then
+  if [ "$with" = "" ]; then
+    usage
+    exit 1
+  fi
+
+  insert "$replace" "$with"
+else
+  while IFS=',' read -r replace with; do
+    insert "$replace" "$with"
+  done < "/dev/stdin"
+fi
+
+#
+# Update primary key in `Z_PRIMARYKEY` table.
+#
+
+sqlite3 "$database" "UPDATE 'Z_PRIMARYKEY' SET Z_MAX = $primary"
+
+#
+# Restart AppleSpell.
+#
+
+killall -e AppleSpell > /dev/null 2>&1
---
+++ readme.md
@@ -0,0 +1,42 @@
+
+# osx-shortcut ![`bin/sh`][bash]
+
+> Add autocorrect text shortcuts, as in, when I type `$a`, its
+> expanded to `$b`.
+
+Tested on OS X 10.10 (Yosemite), might work earlier.
+
+Uses undocumented features, meaning that this might break on new releases
+of OS X (but your shortcuts will not).
+
+## Install
+
+[npm][]
+
+```bash
+npm install osx-shortcut --global
+```
+
+## Usage
+
+```text
+Usage: shortcut [options] <replace> <with>
+
+Options:
+
+  -h, --help     output usage information
+  -v, --version  output version
+
+You can also pass CSV on stdin(4).
+
+Examples:
+
+  $ shortcut "omw" "on my way"
+  $ shortcut <<< "omw,on my way"
+
+See also: man 1 shortcut
+```
+
+Go to `System Preferences` > `Keyboard` > `Text` to see your shortcuts.
+
+![][screenshot]
